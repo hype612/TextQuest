a8caa70dd6a66ab3388ef1a165e02aca71d4548c
yume <keriattila10@gmail.com>
2026-09-04 21:38:28 +0200

    forgot to add rect to last commit

---
+++ src/Headers/Rect.h
@@ -0,0 +1,16 @@
+#ifndef RECT_H
+#define RECT_H
+
+struct Rect {
+  int x;
+  int y;
+  unsigned int width;
+  unsigned int height;
+
+  bool overlaps(const Rect &other) const {
+    return x < other.x + other.width && x + width > other.x &&
+           y < other.y + other.height && y + height > other.y;
+  }
+};
+
+#endif // RECT_H
